4506a038ba29c9a0a14c6a0a10b8841a4fe10ba2
new-marty <yumabuchi1998@gmail.com>
2025-11-03 21:50:17 +0900

    Initial Commit

---
+++ finance-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  finance-app
+//
+//  Created by Yu Mabuchi on 2025/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ finance-app/finance_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  finance_appApp.swift
+//  finance-app
+//
+//  Created by Yu Mabuchi on 2025/11/03.
+//
+
+import SwiftUI
+
+@main
+struct finance_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
